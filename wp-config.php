d9b32ce7a6ba66c9386d4d179a91b24d743bcd5a
Ajay Kumar <ajay011.sharma@hotmail.com>
2019-10-08 12:47:45 +0530

    updating salt variable

--- wp-config.php
+++ wp-config.php
@@ -52,6 +52,10 @@ define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
 define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
 define( 'NONCE_SALT',       'put your unique phrase here' );
 
+/* The value of WPSalts come from the script */
+
+$WPSalts
+
 /**#@-*/
 
 /**
